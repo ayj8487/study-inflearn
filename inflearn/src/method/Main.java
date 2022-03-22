a5de4a2c129c71631d8d14b9f37a832d60b17485
ayj8487 <ayj8487@naver.com>
2022-03-22 22:18:18 +0900

    2022 -03-22 메서드선언과 호출

---
+++ inflearn/src/method/Main.java
@@ -0,0 +1,18 @@
+package method;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		Method method = new Method();
+		
+		method.newInfo("youngJun", "M", 15);
+		
+//		method.name = "youngJun";
+//		method.gender = "M";
+//		method.age = 15;
+		
+		method.info();
+		//메서드 호출부
+	}
+}
---
+++ inflearn/src/method/Method.java
@@ -0,0 +1,39 @@
+package method;
+
+public class Method {
+
+	//메서드 선언과 호출
+	
+	public String name;
+	public String gender;
+	public int age;
+	
+	public void info() { //메서드 선언부
+/*
+	메서드 선언 및 정의
+	
+	public  접근제한자	 :	외부에서 호출을 할수있는지 여부
+	void	반환형	:	데이터의 반환 자료형 명시 
+	info	메서드이름	 :	사용할 메서드이름, 주로 기능을 소문자로 시작해서 짓는다
+	()		매게변수	:	호출부에서 사용	
+*/
+	System.out.println("---getInfo");
+	
+	System.out.println("name : " + name);
+	System.out.println("gender : " + gender);
+	System.out.println("age : " + age);
+	//메서드 정의부
+	
+	}
+	public void newInfo(String n, String g, int a) { // () 매게변수(파라메터)는 필요시 정의되며 자료형과 변수(지역변수)로 이루어진다.
+		System.out.println("---- newInfo ");
+		
+		name = n;
+		gender = g;
+		age = a;
+	}
+	
+	public Method() {
+	System.out.println("생성자");	
+	}
+}
